57c3af44722ffe6864df977a543c6bcf0c930822
Antonio Langella <alangella@unisa.it>
2025-05-16 19:32:58 +0200

    [ADD] Enable rate limiting for joint position, cartesian velocity and pose commands

--- franka_hardware/include/franka_hardware/robot.hpp
+++ franka_hardware/include/franka_hardware/robot.hpp
@@ -315,13 +315,13 @@ class Robot {
   bool torque_command_rate_limiter_active_{true};
   bool velocity_command_rate_limit_active_{false};
 
-  bool cartesian_velocity_command_rate_limit_active_{false};
+  bool cartesian_velocity_command_rate_limit_active_{true};
   bool cartesian_velocity_low_pass_filter_active_{false};
 
   bool cartesian_pose_low_pass_filter_active_{false};
-  bool cartesian_pose_command_rate_limit_active_{false};
+  bool cartesian_pose_command_rate_limit_active_{true};
 
-  bool joint_position_command_rate_limit_active_{false};
+  bool joint_position_command_rate_limit_active_{true};
   bool joint_position_command_low_pass_filter_active_{false};
 
   double low_pass_filter_cut_off_freq{100.0};
