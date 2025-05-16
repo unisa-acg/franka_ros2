a21289f47314bfb86c114a202411c2e04d645a0e
Antonio Langella <alangella@unisa.it>
2025-05-16 11:25:22 +0200

    [REF] Reorder initialization of position commands in FrankaHardwareInterface for clarity

--- franka_hardware/src/franka_hardware_interface.cpp
+++ franka_hardware/src/franka_hardware_interface.cpp
@@ -150,17 +150,18 @@ void initializeCommand(bool& first_update,
 }
 
 void FrankaHardwareInterface::initializePositionCommands(const franka::RobotState& robot_state) {
-  initializeCommand(first_elbow_update_, elbow_command_interface_running_, hw_elbow_command_,
-                    robot_state.elbow_c);
-  initializeCommand(initial_elbow_state_update_, elbow_command_interface_running_,
-                    initial_elbow_state_, robot_state.elbow_c);
   initializeCommand(first_position_update_, position_joint_interface_running_,
                     hw_position_commands_, robot_state.q_d);
   initializeCommand(first_cartesian_pose_update_, pose_cartesian_interface_running_,
                     hw_cartesian_pose_, robot_state.O_T_EE_d);
-  initializeCommand(initial_robot_state_update_, true, initial_robot_pose_, robot_state.O_T_EE_d);
+  initializeCommand(first_elbow_update_, elbow_command_interface_running_, hw_elbow_command_,
+                    robot_state.elbow_c);
+
   initializeCommand(initial_joint_position_update_, true, initial_joint_positions_,
                     robot_state.q_d);
+  initializeCommand(initial_robot_state_update_, true, initial_robot_pose_, robot_state.O_T_EE_d);
+  initializeCommand(initial_elbow_state_update_, elbow_command_interface_running_,
+                    initial_elbow_state_, robot_state.elbow_c);
 }
 
 hardware_interface::return_type FrankaHardwareInterface::read(const rclcpp::Time& /*time*/,
